0a09fb4737cdeac3c6c3132f366402b00770656e
any86 <383514580@qq.com>
2021-03-08 17:23:51 +0800

    wip: 支持swipe滑动

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,48 +1,85 @@
 import AnyTouch from 'any-touch'
+import raf = require('raf')
+console.log(raf);
+
+const STYLE: Partial<CSSStyleDeclaration> = {
+    overflow: 'hidden',
+};
+const CONTENT_STYLE: Partial<CSSStyleDeclaration> = {
+    width: '100%'
+};
 
 export default function (el: HTMLElement, options = {}) {
-    const STYLE: Partial<CSSStyleDeclaration> = {
-        overflow: 'hidden',
-    };
+    // 内容元素当前位移
+    let x = 0;
+    let y = 0;
 
-    const CONTENT_STYLE: Partial<CSSStyleDeclaration> = {
-        width: '100%'
-    };
-
+    // 设置外容器样式
     for (const key in STYLE) {
         el.style.setProperty(key, STYLE[key] as string);
     }
 
-    const contentEL = document.createElement('div');
+    // 生成内容器, 并把外容器内的dom移动到内容器
+    const contentEl = document.createElement('div');
     while (el.firstChild) {
-        contentEL.appendChild(el.firstChild);
+        contentEl.appendChild(el.firstChild);
     }
+    // 设置内容器样式
     for (const key in CONTENT_STYLE) {
-        contentEL.style.setProperty(key, CONTENT_STYLE[key] as string);
+        contentEl.style.setProperty(key, CONTENT_STYLE[key] as string);
     }
-    el.appendChild(contentEL);
+    el.appendChild(contentEl);
 
+    // 加载手势
     const at = new AnyTouch(el);
-    let x = 0;
-    let y = 0;
     at.on('panmove', e => {
         x += e.deltaX;
         y += e.deltaY;
-        scrollTop(contentEL, x, y);
+        contentEl.style.setProperty('transform', `translate3d(${x}px, ${y}px, 0)`);
     });
 
     at.on('swipe', e => {
-        if (1 < e.speedX) {
-            x += e.speedX * 100;
-        }
-
-        if (1 < e.speedY) {
-            y += e.speedY * 100;
-        }
-        scrollTop(contentEL, x, y);
+        let dx = e.speedX * 30;
+        let dy = e.speedY * 30;
+        _scroll(contentEl, [x, y], [dx, dy], 300, ([distX, distY]) => {
+            x = distX;
+            y = distY;
+        });
     });
 }
 
-function scrollTop(el: HTMLElement, x: number, y: number) {
+/**
+ * 设置元素的translate
+ * @param el 元素
+ * @param param1 坐标 
+ */
+function _setContentTranslate(el: HTMLElement, [x, y]: [number, number]) {
     el.style.setProperty('transform', `translate3d(${x}px, ${y}px, 0)`);
+}
+
+function _scroll(el: HTMLElement, [x, y]: [number, number], [dx, dy]: [number, number], duration: number, done: ([x, y]: [number, number]) => void) {
+    let startTime = Date.now();
+    function animate() {
+        const timeDiff = Date.now() - startTime;
+        if (duration > timeDiff) {
+            _setContentTranslate(el, [easeOut(timeDiff, x, dx, duration), easeOut(timeDiff, y, dy, duration)]);
+            raf(animate);
+        } else {
+            const dist = [x + dx, y + dy] as [number, number];
+            _setContentTranslate(el, dist);
+            done && done(dist);
+        }
+        console.log(el.style.transform, x + dx, y + dy);
+    }
+    animate()
+}
+/**
+ * 参考 https://github.com/zhangxinxu/Tween/blob/master/tween.js
+ * t: current time（当前时间）；
+ * b: beginning value（初始值）；
+ * c: change in value（变化量）；
+ * d: duration（持续时间）。
+*/
+function easeOut(t: number, b: number, c: number, d: number) {
+    return -c * (t /= d) * (t - 2) + b;
 }
