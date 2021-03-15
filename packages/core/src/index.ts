3a84e054093e5755762c6704f7650868c4272631
any86 <383514580@qq.com>
2021-03-15 14:37:37 +0800

    wip: 可以暂停拖拽产生的滚动

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
+import dist from 'any-event';
 import AnyTouch from 'any-touch'
 import raf from 'raf';
 
@@ -12,8 +13,9 @@ export default function (el: HTMLElement, options = {}) {
     // 内容元素当前位移
     let x = 0;
     let y = 0;
+    let rafId = 0;
+    let stopFns: [(() => any), (() => any)] = [() => void 0, () => void 0];
 
-    let rafID = 0;
 
     // 设置外容器样式
     for (const key in STYLE) {
@@ -22,6 +24,7 @@ export default function (el: HTMLElement, options = {}) {
 
     // 生成内容器, 并把外容器内的dom移动到内容器
     const contentEl = document.createElement('div');
+
     while (el.firstChild) {
         contentEl.appendChild(el.firstChild);
     }
@@ -31,59 +34,203 @@ export default function (el: HTMLElement, options = {}) {
     }
     el.appendChild(contentEl);
 
+
     // 加载手势
     const at = new AnyTouch(el);
     at.on('panmove', e => {
-        x += e.deltaX;
-        y += e.deltaY;
-        contentEl.style.setProperty('transform', `translate3d(${x}px, ${y}px, 0)`);
+        const { deltaX, deltaY } = e;
+        _setXY([x + deltaX, y + deltaY]);
     });
 
+    at.on('panend', e => {
+        // const [distX, distY] = getResetPostion([el, contentEl], [x, y])
+        // _scrollByTime([el, contentEl], [x, y], [distX - x, distY - y], 300, ([distX, distY]) => {
+        //     x = distX;
+        //     y = distY;
+        // }, id => {
+        //     rafID = id;
+        // });
+    });
 
     at.on('at:start', e => {
-        raf.cancel(rafID)
+        stopFns[0]();
+        stopFns[1]();
+        console.warn(111);
     });
-    
+
     const swipe = at.get('swipe');
-    swipe && swipe.set({velocity:1});
-    
-    console.log(swipe);
+    swipe && swipe.set({ velocity: 1 });
+
     at.on('swipe', e => {
-        let dx = e.speedX * 30;
-        let dy = e.speedY * 30;
-        _scroll(contentEl, [x, y], [dx, dy], 300, ([distX, distY]) => {
-            x = distX;
-            y = distY;
-        }, id => {
-            rafID = id;
-        });
+        let dx = e.speedX * 300;
+        let dy = e.speedY * 300;
+        stopFns = _scrollTo([x + dx, y + dy]);
+        console.warn(stopFns);
     });
+
+    function _setXY([distX, distY]: [number, number]): [number, number] {
+        x = distX;
+        y = distY;
+        contentEl.style.setProperty('transform', `translate3d(${x}px, ${y}px, 0)`);
+        return [x, y];
+    }
+
+    /**
+     * 滚动
+     * @param to 终点
+     */
+    function _scrollTo([distX, distY]: [number, number], onScroll: ([x, y]: [number, number]) => void = (([x, y]) => void 0), damping = 0.1): [() => void, () => void] {
+        const startX = x;
+        const startY = y;
+        let rafIdX = -1;
+        let rafIdY = -1;
+
+        nextTick(distX - startX, (n, rafId) => {
+            rafIdX = rafId;
+            onScroll(_setXY([startX + n, y]));
+        }, damping);
+
+        nextTick(distY - startY, (n, rafId) => {
+            rafIdY = rafId;
+            onScroll(_setXY([x, startY + n]));
+        }, damping);
+
+        return [() => raf.cancel(rafIdX), () => raf.cancel(rafIdY)];
+    }
+}
+
+
+function getValidPostion([el, contentEl]: [HTMLElement, HTMLElement], [x, y]: [number, number], [dx, dy]: [number, number], tolerance: number = 50, damping = 0.5): [number, number] {
+    // 目标坐标
+    let distX = x + dx;
+    let distY = y + dy;
+
+    const MIN_X = el.offsetWidth - contentEl.scrollWidth - tolerance
+    if (MIN_X > distX) {
+        distX = MIN_X;
+    } else if (MIN_X + tolerance > distX && MIN_X <= distX) {
+        distX = x + dx * damping;
+    } else if (0 >= distX) {
+        // distX = Math.max(y, el.offsetWidth - contentEl.scrollWidth - tolerance);
+    } else if (tolerance > distX) {
+        distX = x + dx * damping;
+    } else {
+        distX = tolerance
+    }
+    const MIN_Y = el.offsetHeight - contentEl.scrollHeight - tolerance
+
+    // 超出容差
+    if (MIN_Y > distY) {
+        distY = MIN_Y;
+    }
+    // 底部容差内
+    else if (MIN_Y + tolerance > distY && MIN_Y <= distY) {
+        distY = y + dy * damping;
+    }
+    // 正常范围内
+    else if (0 >= distY) {
+        // distY = Math.max(y, el.offsetHeight - contentEl.scrollHeight - tolerance);
+    }
+    // 上部容差范围内
+    else if (tolerance > distY) {
+        distY = y + dy * damping;
+    }
+    // 超出上部容差
+    else {
+        distY = tolerance;
+    }
+    return [distX, distY];
 }
 
 /**
  * 设置元素的translate
  * @param el 元素
- * @param param1 坐标 
+ * @param start 起始坐标 
+ * @param delatX 坐标变化增量
+ * @param tolerance 容差
+ * @param damping 超过边界的时候变化量的衰减系数(线性关系)
+ * @returns 目标坐标
  */
-function _setContentTranslate(el: HTMLElement, [x, y]: [number, number]) {
-    el.style.setProperty('transform', `translate3d(${x}px, ${y}px, 0)`);
-}
+function _setContentTranslate([el, contentEl]: [HTMLElement, HTMLElement], [x, y]: [number, number], [dx, dy]: [number, number], tolerance: number = 50, damping = 0.5): [number, number] {
+    // 目标坐标
+    let distX = x + dx;
+    let distY = y + dy;
 
-function _scroll(el: HTMLElement, [x, y]: [number, number], [dx, dy]: [number, number], duration: number, onScroll: ([x, y]: [number, number]) => void, onChangeRaf?: (id: number) => void) {
+    const MIN_X = el.offsetWidth - contentEl.scrollWidth - tolerance
+    if (MIN_X > distX) {
+        distX = MIN_X;
+    } else if (MIN_X + tolerance > distX && MIN_X <= distX) {
+        distX = x + dx * damping;
+    } else if (0 >= distX) {
+        // distX = Math.max(y, el.offsetWidth - contentEl.scrollWidth - tolerance);
+    } else if (tolerance > distX) {
+        distX = x + dx * damping;
+    } else {
+        distX = tolerance
+    }
+    const MIN_Y = el.offsetHeight - contentEl.scrollHeight - tolerance
+
+    // 超出容差
+    if (MIN_Y > distY) {
+        distY = MIN_Y;
+        // console.log(1);
+    }
+    // 底部容差内
+    else if (MIN_Y + tolerance > distY && MIN_Y <= distY) {
+        distY = y + dy * damping;
+        // console.log(2);
+    }
+    // 正常范围内
+    else if (0 >= distY) {
+        // console.log(555);
+        // distY = Math.max(y, el.offsetHeight - contentEl.scrollHeight - tolerance);
+    }
+    // 上部容差范围内
+    else if (tolerance > distY) {
+        // console.log(3, distY, tolerance);
+
+        distY = y + dy * damping;
+    }
+    // 超出上部容差
+    else {
+        distY = tolerance;
+        // console.log(4);
+    }
+
+
+
+    contentEl.style.setProperty('transform', `translate3d(${distX}px, ${distY}px, 0)`);
+    return [distX, distY];
+}
+/**
+ * 
+ * @param elements 
+ * @param from 
+ * @param delta 
+ * @param duration 
+ * @param onScroll 
+ * @param onChangeRaf 
+ */
+function _scrollByTime([el, contentEl]: [HTMLElement, HTMLElement], [x, y]: [number, number], deltaXY: [number, number], duration: number, onScroll: ([x, y]: [number, number]) => void, onChangeRaf?: (id: number) => void, onDone?: () => void) {
     let startTime = Date.now();
+
+    const [distX, distY] = getResetPosition([el, contentEl], [x + deltaXY[0], y + deltaXY[1]]);
+    const [dx, dy] = [distX - x, distY - y];
+
     function animate() {
         const timeDiff = Date.now() - startTime;
         if (duration > timeDiff) {
-            const distXY: [number, number] = [easeOut(timeDiff, x, dx, duration), easeOut(timeDiff, y, dy, duration)];
-            _setContentTranslate(el, distXY);
+            const activeXY: [number, number] = [easeOut(timeDiff, x, dx, duration), easeOut(timeDiff, y, dy, duration)];
+            _setContentTranslate([el, contentEl], [x, y], [activeXY[0] - x, activeXY[1] - y], 50, 1);
             onChangeRaf && onChangeRaf(raf(animate));
-            onScroll && onScroll(distXY);
+            onScroll && onScroll(activeXY);
+            console.log(activeXY, timeDiff);
         } else {
-            const distXY = [x + dx, y + dy] as [number, number];
-            _setContentTranslate(el, distXY);
-            onScroll && onScroll(distXY);
+            const activeXY = _setContentTranslate([el, contentEl], [x, y], [dx, dy], 50, 1);
+            onScroll && onScroll(activeXY);
+            onDone && onDone();
+            console.log(activeXY, timeDiff);
         }
-        // console.log(el.style.transform, x + dx, y + dy);
     }
     animate()
 }
@@ -96,4 +243,50 @@ function _scroll(el: HTMLElement, [x, y]: [number, number], [dx, dy]: [number, n
 */
 function easeOut(t: number, b: number, c: number, d: number) {
     return -c * (t /= d) * (t - 2) + b;
+}
+
+/**
+ * 获取最近的边界位置
+ * @param elements 外壳和内容元素
+ * @param postion 当前位置
+ * @returns 边界位置
+ */
+function getResetPosition([el, contentEl]: [HTMLElement, HTMLElement], [x, y]: [number, number]): [number, number] {
+    const MIN_X = el.offsetWidth - contentEl.scrollWidth;
+    const MAX_X = 0;
+    const MIN_Y = el.offsetHeight - contentEl.scrollHeight;
+    const MAX_Y = 0;
+    if (MIN_X > x) {
+        x = MIN_X
+    } else if (MAX_X < x) {
+        x = MAX_X;
+    }
+
+    if (MIN_Y > y) {
+        y = MIN_Y
+    } else if (MAX_Y < y) {
+        y = MAX_Y;
+    }
+    return [x, y];
+};
+
+
+/**
+ * 数字自增
+ * @param total 总数字
+ * @param each 获取当前值的函数
+ * @param damping 衰减系数,范围0~1
+ * @param startValue 起始值
+ */
+function nextTick(total: number, each: (n: number, rafId: number) => void, damping: number, startValue = 0) {
+    let rafId = -1;
+    startValue += damping * (total - startValue);
+    if (0.1 < Math.abs(total - startValue)) {
+        rafId = raf(() => {
+            nextTick(total, each, damping, startValue);
+        });
+    } else {
+        startValue = total;
+    }
+    each(startValue, rafId);
 }
