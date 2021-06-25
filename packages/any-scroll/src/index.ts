8645a0cd7f15dcbf460a56ea8f4e185984fd98e5
Russell <383514580@qq.com>
2021-06-25 16:06:39 +0800

    refactor: AnyScroll支持选项配置

--- packages/any-scroll/src/index.ts
+++ packages/any-scroll/src/index.ts
@@ -1,7 +1,7 @@
 import Core from '@any-scroll/core'
-
+interface Options { tolerance: number, damping: number, allow: [boolean, boolean] };
 export default class {
-    constructor(el: HTMLElement) {
-        return new Core(el);
+    constructor(el: HTMLElement,options:Options) {
+        return new Core(el,options);
     }
 }
